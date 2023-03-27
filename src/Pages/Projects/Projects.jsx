42190bc57fddf7e9001fbf4d7eea46586b9bd206
Your Name~manuel <manuelcreation247@gmail.com>
2023-03-27 13:15:11 +0100

    gg

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -11,14 +11,14 @@ const Projects = () => {
           <div class="row">
             <div class="col-lg-12">
               <div class="breadcrumb-content">
-                <h2 class="title">Projects Masonry</h2>
+                <h2 class="title">Services Page</h2>
                 <nav aria-label="breadcrumb">
                   <ol class="breadcrumb">
                     <li class="breadcrumb-item">
                       <a href="index.html">Home</a>
                     </li>
                     <li class="breadcrumb-item active" aria-current="page">
-                      Projects Masonry
+                      Services
                     </li>
                   </ol>
                 </nav>
