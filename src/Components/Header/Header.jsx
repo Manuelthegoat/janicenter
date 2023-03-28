a801bd23e1c1894ed277f82648fd103e16d911be
Your Name~manuel <manuelcreation247@gmail.com>
2023-03-28 20:34:18 +0100

    gg

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../Img/logo2.png";
 import "./Header.css";
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Header = () => {
   const [sticky, setSticky] = useState(false);
-//   const [mobile, setMoblie] = useState(true);
+  //   const [mobile, setMoblie] = useState(true);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -21,19 +23,31 @@ const Header = () => {
   });
 
   return (
-  
-    <Navbar expand="sm" className={`${sticky ? "sticky" : "nav"}`}>
+    <Navbar collapseOnSelect expand="lg" className={`${sticky ? "sticky " : "nav"}`}>
     <Container>
       <Navbar.Brand href="#home"><img src="logo4.svg" className="logos" alt="" /></Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">About</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+         
         </Nav>
+        <Nav className="mpt">
+          <Nav.Link className='text-light emmas' href="#deets">Home</Nav.Link>
+          <Nav.Link className='text-light emmas' href="#deets">About Us</Nav.Link>
+          <Nav.Link className='text-light emmas' href="#deets">Our Partners</Nav.Link>
+          <Nav.Link className='text-light emmas' href="#deets">Become An Achiever</Nav.Link>
+          <Nav.Link className='text-light emmas' href="#deets">Customer Service</Nav.Link>
+         
+        </Nav>
+        <div className="nav-btn">
+            <div className="icon">
+                <i className="fas fa-phone-alt"></i>
+            </div>
+            <div className="content">
+                <p>Call for help</p>
+                <a>(+234) 909 532 1394</a>
+            </div>
+          </div>
       </Navbar.Collapse>
     </Container>
   </Navbar>
