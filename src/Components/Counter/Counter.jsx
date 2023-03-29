ea7e8cb27fae26e11a6e6039f201139aec083eb2
Your Name~manuel <manuelcreation247@gmail.com>
2023-03-29 13:47:34 +0100

    gg

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -8,19 +8,19 @@ const Counter = () => {
                     <div class="row">
                         <div class="col-lg-6 col-sm-6">
                             <div class="counter-item">
-                                <div class="icon">
+                                <div class="icons">
                                     {/* <svg viewBox="0 0 65 65" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M32.5 40.6251C42.5965 40.6251 50.7812 32.7434 50.7812 23.0209C50.7812 13.2984 42.5965 5.41675 32.5 5.41675C22.4035 5.41675 14.2188 13.2984 14.2188 23.0209C14.2188 32.7434 22.4035 40.6251 32.5 40.6251Z" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                                         <path d="M20.3666 36.6164L20.3396 56.6039C20.3396 59.0414 22.0459 60.2331 24.1584 59.231L31.4167 55.7914C32.0125 55.4935 33.0146 55.4935 33.6104 55.7914L40.8959 59.231C42.9813 60.206 44.7146 59.0414 44.7146 56.6039V36.1289" stroke="currentcolor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
                                     </svg> */}
-                                    <img src="assets/img/appstore.png" alt="" />
+                                    <img src="assets/img/appstore.png"  alt="" />
                                 </div>
                                
                             </div>
                         </div>
                         <div class="col-lg-6 col-sm-6">
                             <div class="counter-item">
-                                <div class="icon">
+                                <div class="icons">
                                 <img src="assets/img/playstore.png" alt="" />
 
                                 </div>
