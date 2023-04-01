a3195dd1ce7ceff6f0b784c03c93566fc098f200
Your Name~manuel <manuelcreation247@gmail.com>
2023-04-01 07:07:59 +0100

    gg

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,86 +1,96 @@
-import React from 'react'
+import React from "react";
 
 const Testimonials = () => {
   return (
     <div>
       <section class="testimonial-area-four  pb-100">
-                <div class="container">
-                <div class="row justify-content-center">
-                        <div class="col-lg-8 col-md-10">
-                            <div class="section-title-three text-center">
-                                {/* <span class="sub-title">Our Services</span> */}
-                                <h2 class="title">Testimonials And Reviews</h2>
-                                <p>Commodo dictum iaculis eget mas phasellus ultrices nunc dignissim. Id nulla amet tincidunt urna sed massa</p>
-                            </div>
-                        </div>
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-8 col-md-10">
+              <div class="section-title-three text-center">
+                {/* <span class="sub-title">Our Services</span> */}
+                <h2 class="title">Testimonials And Reviews</h2>
+                <p>
+                  Commodo dictum iaculis eget mas phasellus ultrices nunc
+                  dignissim. Id nulla amet tincidunt urna sed massa
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="row testimonial-active">
+            <div class="col-lg-4">
+              <div class="testimonial-item">
+                {/* <div class="testimonial-icon">
+                                    <i class="fas fa-quote-right"></i>
+                                </div> */}
+                <div class="testimonial-content">
+                  <p>
+                    “The cleaning was well executed and I am very satisfied with
+                    the service delivery.”
+                  </p>
+                  <div class="testimonial-avatar-info">
+                    <div class="avatar-thumb">
+                      <img src="assets/img/images/avatar_img01.png" alt="" />
                     </div>
-                    <div class="row testimonial-active">
-                        <div class="col-lg-4">
-                            <div class="testimonial-item">
-                                {/* <div class="testimonial-icon">
-                                    <i class="fas fa-quote-right"></i>
-                                </div> */}
-                                <div class="testimonial-content">
-                                    <p>“The cleaning was well executed and 
-I am very satisfied with the service delivery.”</p>
-                                    <div class="testimonial-avatar-info">
-                                        <div class="avatar-thumb">
-                                            <img src="assets/img/images/avatar_img01.png" alt=""/>
-                                        </div>
-                                        <div class="avatar-content">
-                                            <h2 class="title">Residential Client,</h2>
-                                            <p>Parkview - FeedBack</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="testimonial-item">
-                                {/* <div class="testimonial-icon">
-                                    <i class="fas fa-quote-right"></i>
-                                </div> */}
-                                <div class="testimonial-content">
-                                    <p> “Janicenter rendered quality, professional 
-Service. The workers were prompt and their service was excellent.”</p>
-                                    <div class="testimonial-avatar-info">
-                                        <div class="avatar-thumb">
-                                            <img src="assets/img/images/avatar_img02.png" alt=""/>
-                                        </div>
-                                        <div class="avatar-content">
-                                            <h2 class="title">Chapel Hill Denham</h2>
-                                            <p>Feedback</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-4">
-                            <div class="testimonial-item">
-                                {/* <div class="testimonial-icon">
-                                    <i class="fas fa-quote-right"></i>
-                                </div> */}
-                                <div class="testimonial-content">
-                                    <p>“You and your team did a good job! I am 
-particularly impressed with your excellent disposition to CUSTOMER 
-SERVICE…. Well Done!”</p>
-                                    <div class="testimonial-avatar-info">
-                                        <div class="avatar-thumb">
-                                            <img src="assets/img/images/avatar_img01.png" alt=""/>
-                                        </div>
-                                        <div class="avatar-content">
-                                            <h2 class="title">Mr. Martins</h2>
-                                            <p> Bank Executive </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                    <div class="avatar-content">
+                      <h2 class="title">Residential Client,</h2>
+                      <p>Parkview - FeedBack</p>
                     </div>
+                  </div>
                 </div>
-            </section>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="testimonial-item">
+                {/* <div class="testimonial-icon">
+                                    <i class="fas fa-quote-right"></i>
+                                </div> */}
+                <div class="testimonial-content">
+                  <p>
+                    {" "}
+                    “Janicenter rendered quality, professional Service. The
+                    workers were prompt and their service was excellent.”
+                  </p>
+                  <div class="testimonial-avatar-info">
+                    <div class="avatar-thumb">
+                      <img src="assets/img/images/avatar_img02.png" alt="" />
+                    </div>
+                    <div class="avatar-content">
+                      <h2 class="title">Chapel Hill Denham</h2>
+                      <p>Feedback</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4">
+              <div class="testimonial-item">
+                {/* <div class="testimonial-icon">
+                                    <i class="fas fa-quote-right"></i>
+                                </div> */}
+                <div class="testimonial-content">
+                  <p>
+                    “You and your team did a good job! I am particularly
+                    impressed with your excellent disposition to CUSTOMER
+                    SERVICE…. Well Done!”
+                  </p>
+                  <div class="testimonial-avatar-info">
+                    <div class="avatar-thumb">
+                      <img src="assets/img/images/avatar_img01.png" alt="" />
+                    </div>
+                    <div class="avatar-content">
+                      <h2 class="title">Mr. Martins</h2>
+                      <p> Bank Executive </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
