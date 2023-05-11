5564da89bcf08944fa6445398521e4da4afe6418
Codefunky <108434941+Manuelthegoat@users.noreply.github.com>
2023-05-11 17:16:37 +0100

    Update Achiever.jsx

--- src/Pages/Achiever.jsx
+++ src/Pages/Achiever.jsx
@@ -36,15 +36,14 @@ const Achiever = () => {
                 <div class="project-details-content">
                   <h2 class="title">Our Achievers</h2>
    
-                  <p>
-                   Our achievers are professional cleaners and janitors who maintain buildings, facilities, and other commercial or residential spaces. Our Achievers don't only work in a variety of settings, including office buildings, schools, hospitals, airports, hotels, and more but are able to identify and report any maintenance or repair needs, such as broken light fixtures or leaky faucets 
+                  <p> Our achievers are professional cleaners and janitors who maintain buildings, facilities, and other commercial or residential spaces. Our Achievers don't only work in a variety of settings, including office buildings, schools, hospitals, airports, hotels, and more but are able to identify and report any maintenance or repair needs, such as broken light fixtures or leaky faucets </p>            
                   <img src="assets/img/achiever.jpg" alt="" />
-                  <br/>
+                   
                   <br/>
                   Our Achievers are trained to perform basic maintenance tasks, such as changing air filters or replacing light bulbs and are also able maintain outdoor areas, such as sidewalks, parking lots, and landscaping.
                   <br/>
                   <br/>
-                  </p>
+               
 
                   <h4 class="title-two">Become An Achiever</h4>
                   <br/>
